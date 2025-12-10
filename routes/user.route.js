c230caa4ecdf928b3399e81cad07cee62a7001c3
Yash Thorat <yashthorat1912@gmail.com>
2025-12-10 20:06:34 +0530

    Added api to update user name and Email

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,19 @@
-import { login, register, searchUser } from "../controller/auth.controller.js"
-import express from 'express'
-import { getMyProfileData, uploadProfilePic } from "../controller/user.controller.js";
-import {authMiddleware} from "../utility/auth.Middleware.js";
+import { login, register, searchUser } from "../controller/auth.controller.js";
+import express from "express";
+import {
+  getMyProfileData,
+  uploadProfilePic,
+} from "../controller/user.controller.js";
+import { authMiddleware } from "../utility/auth.Middleware.js";
 import { upload } from "../utility/multer.js";
 import { getUserWithPosts } from "../controller/user.controller.js";
-const router = express.Router()
+import { updateUserDetails } from "../controller/user.controller.js";
+const router = express.Router();
 
-router.post("/register",register)
-router.post("/login",login)
-router.get("/search/:name",searchUser)
-router.get("/me",authMiddleware,getMyProfileData)
+router.post("/register", register);
+router.post("/login", login);
+router.get("/search/:name", searchUser);
+router.get("/me", authMiddleware, getMyProfileData);
 router.get("/:id/fullprofile", authMiddleware, getUserWithPosts);
 router.post(
   "/uploadProfilePic",
@@ -17,4 +21,5 @@ router.post(
   upload.single("profilePic"),
   uploadProfilePic
 );
+router.put("/update", authMiddleware, updateUserDetails);
 export default router;
