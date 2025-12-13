b9b112ffc76d5f4d8ac4fb9ccd654d72b98ecd6c
Raja18110 <rkumari05195rr@gmail.com>
2025-12-13 19:32:45 +0530

    add fetch deletedposts

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,13 +1,14 @@
-import { createPost,getMyPosts,likePost,upload,deletePost,restorePost } from "../controller/post.controller.js";
+import { createPost, getMyPosts, likePost, upload, deletePost, restorePost, getDeletedPosts } from "../controller/post.controller.js";
 import express from 'express'
 import { authMiddleware } from "../utility/auth.Middleware.js";
 
 const router = express.Router();
 
-router.post("/create",authMiddleware,upload("image"),createPost)
-router.post("/like/:id",authMiddleware,likePost)
-router.get("/myposts",authMiddleware,getMyPosts)
+router.post("/create", authMiddleware, upload("image"), createPost);
+router.post("/like/:id", authMiddleware, likePost);
+router.get("/myposts", authMiddleware, getMyPosts);
 router.delete("/delete/:id", authMiddleware, deletePost);
 router.put("/restore/:id", authMiddleware, restorePost);
+router.get('/deletedposts', authMiddleware, getDeletedPosts);
 
-export default router
+export default router;
